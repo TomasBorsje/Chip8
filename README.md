9d5acde8991ba977149274500f2415544599d294
TomasBorsje <t.borsje2@gmail.com>
2026-05-05 23:02:58 +1200

    feat: Parse more opcodes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Rust Chip8 Emulator
-Written in Rust and compiled to wasm to run in the browser.
+Written in Rust and compiled to Wasm to run in the browser.
 
 ### Credits
 Big thanks to [Tobias V. I. Langhoff's high-level Chip8 spec](https://tobiasvl.github.io/blog/write-a-chip-8-emulator/).
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use std::io::Error;
-
+use std::collections::HashSet;
 use wasm_bindgen::prelude::*;
 
 const PIXEL_COLUMNS: usize = 64;
@@ -7,23 +6,36 @@ const PIXEL_ROWS: usize = 32;
 const SCREEN_SIZE: usize = PIXEL_COLUMNS * PIXEL_ROWS; // 64 columns, 32 rows
 const PROG_STARTING_ADDRESS: u16 = 0x200;
 const FONT_SET: [u8; 80] = [
-    0xF0, 0x90, 0x90, 0x90, 0xF0,
-    0x20, 0x60, 0x20, 0x20, 0x70,
-    0xF0, 0x10, 0xF0, 0x80, 0xF0,
-    0xF0, 0x10, 0xF0, 0x10, 0xF0,
-    0x90, 0x90, 0xF0, 0x10, 0x10,
-    0xF0, 0x80, 0xF0, 0x10, 0xF0,
-    0xF0, 0x80, 0xF0, 0x90, 0xF0,
-    0xF0, 0x10, 0x20, 0x40, 0x40,
-    0xF0, 0x90, 0xF0, 0x90, 0xF0,
-    0xF0, 0x90, 0xF0, 0x10, 0xF0,
-    0xF0, 0x90, 0xF0, 0x90, 0x90,
-    0xE0, 0x90, 0xE0, 0x90, 0xE0,
-    0xF0, 0x80, 0x80, 0x80, 0xF0,
-    0xE0, 0x90, 0x90, 0x90, 0xE0,
-    0xF0, 0x80, 0xF0, 0x80, 0xF0,
-    0xF0, 0x80, 0xF0, 0x80, 0x80,
+    0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10, 0xF0, 0x80, 0xF0, 0xF0,
+    0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10, 0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, 0xF0, 0x80,
+    0xF0, 0x90, 0xF0, 0xF0, 0x10, 0x20, 0x40, 0x40, 0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0,
+    0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0, 0xF0, 0x80, 0x80, 0x80,
+    0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, 0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80,
 ];
+
+enum OpCode {
+    ClearScreen,                             // 00E0
+    PopSubroutine,                           // 00EE
+    Jump { nnn: u16 },                       // 1NNN
+    CallSubroutine { nnn: u16 },             // 2NNN
+    SkipIfEqual { x: u8, nn: u8 },           // 3XNN
+    SkipIfNotEqual { x: u8, nn: u8 },        // 4XNN
+    SkipIfRegisterEqual { x: u8, y: u8 },    // 5XY0
+    SkipIfRegisterNotEqual { x: u8, y: u8 }, // 9XY0
+    SetRegister { x: u8, nn: u8 },           // 6XNN
+    AddValueToRegister { x: u8, nn: u8 },    // 7XNN
+    SetIndexRegister { nnn: u16 },           // ANNN
+    Draw { x: u8, y: u8, n: u8 },            // DXYN
+    Set { x: u8, y: u8 },                    // 8XY0
+    BinaryOr { x: u8, y: u8 },               // 8XY1
+    BinaryAnd { x: u8, y: u8 },              // 8XY2
+    LogicalXor { x: u8, y: u8 },             // 8XY3
+    Add { x: u8, y: u8 },                    // 8XY4
+    SubtractXY { x: u8, y: u8 },             // 8XY5
+    SubtractYX { x: u8, y: u8 },             // 8XY7
+    ShiftRight { x: u8, y: u8 },             // 8XY6
+    ShiftLeft { x: u8, y: u8 },              // 8XYE
+}
 
 #[wasm_bindgen]
 #[derive(Debug)]
@@ -35,21 +47,23 @@ pub struct Chip8Machine {
     delay_timer: u8,
     sound_timer: u8,
     registers: [u8; 16],
+    current_input: HashSet<u8>,
     display: [bool; SCREEN_SIZE],
 }
 
 #[wasm_bindgen]
 impl Chip8Machine {
     pub fn new(program: &[u8]) -> Option<Chip8Machine> {
-        let mut chip_machine = Chip8Machine { 
-            memory: [0; 4096], 
+        let mut chip_machine = Chip8Machine {
+            memory: [0; 4096],
             program_counter: PROG_STARTING_ADDRESS,
             index_register: 0,
             stack: vec![],
             delay_timer: 0,
             sound_timer: 0,
             registers: [0; 16],
-            display: [false; 64*32],
+            current_input: HashSet::new(),
+            display: [false; 64 * 32],
         };
         chip_machine.memory[0x050..0x0A0].copy_from_slice(&FONT_SET); // Copy font set into memory
 
@@ -72,8 +86,12 @@ impl Chip8Machine {
         }
     }
 
-    pub fn input(key: &str) {
-        todo!();
+    pub fn input_down(&mut self, key: u8) {
+        self.current_input.insert(key);
+    }
+
+    pub fn input_up(&mut self, key: u8) {
+        self.current_input.remove(&key);
     }
 
     pub fn cycle(&mut self) {
@@ -81,7 +99,9 @@ impl Chip8Machine {
         let (instruction_one, instruction_two) = self.fetch();
 
         // Decode the instruction
-        let operation = self.decode(instruction_one, instruction_two).expect("Unknown OpCode number");
+        let operation = self
+            .decode(instruction_one, instruction_two)
+            .expect("Unknown OpCode number");
 
         // Execute the instruction
         self.execute(operation);
@@ -102,16 +122,38 @@ impl Chip8Machine {
         let y: u8 = (instruction_two >> 4) & 0b_00001111; // Register 2 addr
         let n: u8 = instruction_two & 0b_00001111;
         let nn: u8 = instruction_two;
-        let nnn: u16 = 0b_0000000000000000 + ((x as u16) << 8) + nn as u16;
+        let nnn: u16 = ((x as u16) << 8) + nn as u16;
 
         match instruction_code {
-            0x0 => Ok(OpCode::ClearScreen),
+            0x0 => match (x, n) {
+                (0x0, 0x0) => Ok(OpCode::ClearScreen),
+                (0x0, 0xE) => Ok(OpCode::PopSubroutine),
+                (_, _) => panic!("Unrecognised 00E_ opcode with nibble {n}"),
+            },
             0x1 => Ok(OpCode::Jump { nnn }),
+            0x2 => Ok(OpCode::CallSubroutine { nnn }),
+            0x3 => Ok(OpCode::SkipIfEqual { x, nn }),
+            0x4 => Ok(OpCode::SkipIfNotEqual { x, nn }),
+            0x5 => Ok(OpCode::SkipIfRegisterEqual { x, y }),
             0x6 => Ok(OpCode::SetRegister { x, nn }),
             0x7 => Ok(OpCode::AddValueToRegister { x, nn }),
+            0x8 => match n {
+                0x0 => Ok(OpCode::Set { x, y }),
+                0x1 => Ok(OpCode::BinaryOr { x, y }),
+                0x2 => Ok(OpCode::BinaryAnd { x, y }),
+                0x3 => Ok(OpCode::LogicalXor { x, y }),
+                0x4 => Ok(OpCode::Add { x, y }),
+                0x5 => Ok(OpCode::SubtractXY { x, y }),
+                0x6 => Ok(OpCode::ShiftLeft { x, y }),
+                0x7 => Ok(OpCode::SubtractYX { x, y }),
+                0xE => Ok(OpCode::ShiftRight { x, y }),
+                _ => panic!("Unrecognised 8XY_ opcode with nibble {n}"),
+            },
+            0x9 => Ok(OpCode::SkipIfRegisterNotEqual { x, y }),
             0xA => Ok(OpCode::SetIndexRegister { nnn }),
             0xD => Ok(OpCode::Draw { x, y, n }),
-            _ => panic!("Could not decode instruction with ID {0}", instruction_code)
+
+            _ => panic!("Could not decode instruction with ID {0}", instruction_code),
         }
     }
 
@@ -119,19 +161,19 @@ impl Chip8Machine {
         match operation {
             OpCode::ClearScreen => {
                 self.display.fill(false);
-            },
+            }
             OpCode::Jump { nnn } => {
                 self.program_counter = nnn;
-            },
+            }
             OpCode::SetRegister { x, nn } => {
                 self.registers[x as usize] = nn;
-            },
+            }
             OpCode::AddValueToRegister { x, nn } => {
                 self.registers[x as usize] += nn;
-            },
+            }
             OpCode::SetIndexRegister { nnn } => {
                 self.index_register = nnn;
-            },
+            }
             OpCode::Draw { x, y, n } => {
                 let x_coord = self.registers[x as usize] % PIXEL_COLUMNS as u8;
                 let y_coord = self.registers[y as usize] % PIXEL_ROWS as u8;
@@ -140,27 +182,32 @@ impl Chip8Machine {
                 // Draw the sprite, setting 0xF to 1 if anything was turned off by this
                 for row in 0..n {
                     let pixel_y = y_coord + row;
-                    if pixel_y as usize >= PIXEL_ROWS { break; }
+                    if pixel_y as usize >= PIXEL_ROWS {
+                        break;
+                    }
 
                     let sprite_row = self.memory[(self.index_register + row as u16) as usize];
                     for bit in 0..8 {
-                        let mask: u8 = (2u8).pow(7-bit);
+                        let mask: u8 = (2u8).pow(7 - bit);
                         let enable_pixel: bool = (sprite_row) | mask == sprite_row;
 
                         let pixel_x = x_coord + bit as u8;
-                        if pixel_x as usize >= PIXEL_COLUMNS { continue; } // Stop if we're about to go off screen
+                        if pixel_x as usize >= PIXEL_COLUMNS {
+                            continue;
+                        } // Stop if we're about to go off screen
 
                         let pixel_index = pixel_y as usize * PIXEL_ROWS + pixel_x as usize;
 
                         if enable_pixel {
-                            if self.display[pixel_index] { // If we're about to turn off a pixel, set 0xF to 1
+                            if self.display[pixel_index] {
+                                // If we're about to turn off a pixel, set 0xF to 1
                                 self.registers[0xF] = 1;
                             }
                             self.display[pixel_index] = !self.display[pixel_index];
                         }
                     }
                 }
-            },
+            }
         }
     }
 
@@ -169,7 +216,7 @@ impl Chip8Machine {
 
         for row in 0..PIXEL_ROWS {
             for col in 0..PIXEL_COLUMNS {
-                output.push(match self.display[row*PIXEL_COLUMNS+col] {
+                output.push(match self.display[row * PIXEL_COLUMNS + col] {
                     true => '■',
                     false => '□',
                 });
@@ -178,13 +225,4 @@ impl Chip8Machine {
         }
         output
     }
-}
-
-enum OpCode {
-    ClearScreen, // 00E0
-    Jump{nnn: u16}, // 1NNN
-    SetRegister{x: u8, nn: u8}, // 6XNN
-    AddValueToRegister{x: u8, nn: u8}, // 7XNN
-    SetIndexRegister{nnn: u16}, // ANNN
-    Draw{x: u8, y: u8, n: u8}, // DXYN
 }
